e917ec0172509a6976a7e0a72a4503e553ec3f74
BS1594 <siraj.salehin@brainstation-23.com>
2024-06-13 17:12:51 +0600

    Before Each Added to Top

--- tests/myFirst.spec.js
+++ tests/myFirst.spec.js
@@ -1,14 +1,16 @@
 const { test, expect } = require("@playwright/test");
 
+test.beforeEach(async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/.*Swag Labs/);
+});
+
 test.describe("Login Five Test Cases", () => {
   test("Negative Testcase| wrong username, correct password", async ({
     page,
   }) => {
-    await page.goto("https://www.saucedemo.com/");
-
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/.*Swag Labs/);
-
     await page.locator("[id = user-name]").fill("standard_use");
     await page.locator("[id = password]").fill("secret_sauce");
     await page.getByRole("button", { name: "Login" }).click();
@@ -20,11 +22,6 @@ test.describe("Login Five Test Cases", () => {
   test("Negative Testcase| correct username, wrong password", async ({
     page,
   }) => {
-    await page.goto("https://www.saucedemo.com/");
-
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/.*Swag Labs/);
-
     await page.locator("[id = user-name]").fill("standard_user");
     await page.locator("[id = password]").fill("secretsauce");
     await page.getByRole("button", { name: "Login" }).click();
@@ -36,11 +33,6 @@ test.describe("Login Five Test Cases", () => {
   test("Negative Testcase| wrong username, wrong password", async ({
     page,
   }) => {
-    await page.goto("https://www.saucedemo.com/");
-
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/.*Swag Labs/);
-
     await page.locator("[id = user-name]").fill("standard_use");
     await page.locator("[id = password]").fill("secretsauce");
     await page.getByRole("button", { name: "Login" }).click();
@@ -50,10 +42,6 @@ test.describe("Login Five Test Cases", () => {
   });
 
   test("Negative Testcase| empty fields, click submit", async ({ page }) => {
-    await page.goto("https://www.saucedemo.com/");
-
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/.*Swag Labs/);
     //await page.locator("[id = user-name]").fill("standard_use");
     //await page.locator("[id = password]").fill("secret_sauce");
     await page.getByRole("button", { name: "Login" }).click();
@@ -63,11 +51,6 @@ test.describe("Login Five Test Cases", () => {
   });
 
   test("Postive Testcase", async ({ page }) => {
-    await page.goto("https://www.saucedemo.com/");
-
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/.*Swag Labs/);
-
     await page.locator("[id = user-name]").fill("standard_user");
     await page.locator("[id = password]").fill("secret_sauce");
     await page.getByRole("button", { name: "Login" }).click();
@@ -76,4 +59,6 @@ test.describe("Login Five Test Cases", () => {
     // await new Promise(() => {}); // prevents your script from exiting!
     // await browser.close();
   });
+
+ 
 });
