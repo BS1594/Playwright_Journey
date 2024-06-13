98ea0166e126934294aa9f5b1f3909822cb409da
BS1594 <siraj.salehin@brainstation-23.com>
2024-06-13 15:27:08 +0600

    Login Five Test cases + A new exmp file

---
+++ tests/myFirst.spec.js
@@ -0,0 +1,79 @@
+const { test, expect } = require("@playwright/test");
+
+test.describe("Login Five Test Cases", () => {
+  test("Negative Testcase| wrong username, correct password", async ({
+    page,
+  }) => {
+    await page.goto("https://www.saucedemo.com/");
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/.*Swag Labs/);
+
+    await page.locator("[id = user-name]").fill("standard_use");
+    await page.locator("[id = password]").fill("secret_sauce");
+    await page.getByRole("button", { name: "Login" }).click();
+    await expect(page.locator(".error-message-container")).toHaveText(
+      "Epic sadface: Username and password do not match any user in this service"
+    );
+  });
+
+  test("Negative Testcase| correct username, wrong password", async ({
+    page,
+  }) => {
+    await page.goto("https://www.saucedemo.com/");
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/.*Swag Labs/);
+
+    await page.locator("[id = user-name]").fill("standard_user");
+    await page.locator("[id = password]").fill("secretsauce");
+    await page.getByRole("button", { name: "Login" }).click();
+    await expect(page.locator(".error-message-container")).toHaveText(
+      "Epic sadface: Username and password do not match any user in this service"
+    );
+  });
+
+  test("Negative Testcase| wrong username, wrong password", async ({
+    page,
+  }) => {
+    await page.goto("https://www.saucedemo.com/");
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/.*Swag Labs/);
+
+    await page.locator("[id = user-name]").fill("standard_use");
+    await page.locator("[id = password]").fill("secretsauce");
+    await page.getByRole("button", { name: "Login" }).click();
+    await expect(page.locator(".error-message-container")).toHaveText(
+      "Epic sadface: Username and password do not match any user in this service"
+    );
+  });
+
+  test("Negative Testcase| empty fields, click submit", async ({ page }) => {
+    await page.goto("https://www.saucedemo.com/");
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/.*Swag Labs/);
+    //await page.locator("[id = user-name]").fill("standard_use");
+    //await page.locator("[id = password]").fill("secret_sauce");
+    await page.getByRole("button", { name: "Login" }).click();
+    await expect(page.locator(".error-message-container")).toHaveText(
+      "Epic sadface: Username is required"
+    );
+  });
+
+  test("Postive Testcase", async ({ page }) => {
+    await page.goto("https://www.saucedemo.com/");
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/.*Swag Labs/);
+
+    await page.locator("[id = user-name]").fill("standard_user");
+    await page.locator("[id = password]").fill("secret_sauce");
+    await page.getByRole("button", { name: "Login" }).click();
+    await expect(page.locator(".title")).toHaveText("Products");
+
+    // await new Promise(() => {}); // prevents your script from exiting!
+    // await browser.close();
+  });
+});
