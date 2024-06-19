cb98983db97d61359e68d98014decc10d33613ef
BS1594 <siraj.salehin@brainstation-23.com>
2024-06-19 13:05:53 +0600

    Complete Purchase Item + Modified scirpt

--- tests/myFirst.spec.js
+++ tests/myFirst.spec.js
@@ -1,14 +1,20 @@
 const { test, expect } = require("@playwright/test");
 const { syncBuiltinESMExports } = require("module");
 
-test.beforeEach(async ({ page }) => {
+test.beforeEach(async ({ page }, testInfo) => {
   await page.goto("https://www.saucedemo.com/");
+  
+  console.log(`Running  ${testInfo.title}`);
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/.*Swag Labs/);
 });
 
-test.describe.skip("Login Five Test Cases", () => {
+test.afterEach( async ({page}, testInfo) =>{
+  console.log(`Finished ${testInfo.title} with status ${testInfo.status}`);
+})
+
+test.describe("Login Five Test Cases", () => {
   test("Test Scenario 1| wrong username, correct password", async ({
     page,
   }) => {
@@ -58,7 +64,7 @@ test.describe.skip("Login Five Test Cases", () => {
   });
 });
 
-test.describe("Add to Cart the least price item", () => {
+test.describe("Purchase the lowest price item", () => {
   test("Test Scenario 1", async ({ page }) => {
     //await page.pause();
     await page.locator("[id = user-name]").fill("standard_user");
@@ -69,35 +75,38 @@ test.describe("Add to Cart the least price item", () => {
     const itemList = await page
       .locator("data-test=inventory-item-price")
       .allTextContents();
-    
+
     var min = Number.MAX_VALUE;
     let arraValue = 0.0;
     let index = 0;
     for (let i in itemList) {
-      arraValue = eval(itemList[i].replace('$', ''));
-      if (min > arraValue){
+      arraValue = eval(itemList[i].replace("$", ""));
+      if (min > arraValue) {
         min = arraValue;
         index = i;
       }
     }
     console.log(min);
 
-    const lstItemBtn = await page.locator('button').all();
+    const lstItemBtn = await page.locator("button").all();
     await lstItemBtn[index].click();
 
-    await page.locator('.shopping_cart_link').click();
-    await page.getByRole('button', {name : 'Checkout'}).click();
-    const formList = await page.locator('.form_input');
+    await page.locator(".shopping_cart_link").click();
+    await page.getByRole("button", { name: "Checkout" }).click();
 
+    const addressForm1 = await page.getByRole("textbox").all();
+
+    await addressForm1[0].fill("john");
+    await addressForm1[1].fill("Doe");
+    await addressForm1[2].fill("8200");
+    await page.locator('id=continue').click();
+    await expect(page.locator(".title")).toHaveText("Checkout: Overview");
+
+    console.log(await page.locator('data-test=payment-info-value').textContent());
+
+    await page.locator('id=finish').click();
+
+    await expect(page.locator('data-test=complete-header')).toHaveText('Thank you for your order!');
     
-    
-    //console.log(formList);
-     //await formList[0].fill("john");
-    // await formList[1].fill("Doe");
-    // await formList[2].fill("8200");
-
-    // await page.locator('id=continue').click();
-
-
   });
 });
